41a48a3409b6cb6b38e05f660c184b6ab868bb14
Gary Ranson <garyranson@hotmail.com>
2021-05-06 13:30:20 +0100

    Creates Test Harness

---
+++ TestConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Snowflake.Data.Client;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+
+        {
+            Console.WriteLine("Hello World!");
+            var cs =
+                "";
+            var c = new SnowflakeDbConnection() { ConnectionString = cs};
+            await c.OpenAsync();
+            await c.CloseAsync();
+        }
+    }
+}
